44f01e20d923cac8037ba10639c0c9c79fda0f25
RintaroNasu <rinta022823@gmail.com>
2024-11-01 23:10:03 +0900

    プロジェクト作成

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center text-[1.875rem] font-semibold text-[#827f82]">Yumemi Graph App</div>
+    </>
+  );
+}
