be34192393d5984a68310fc9749c2b9871bd7b0e
RintaroNasu <rinta022823@gmail.com>
2024-11-02 15:24:09 +0900

    県ごとの人口の取得の実装をしました。

--- src/utils/getResasApi.ts
+++ src/utils/getResasApi.ts
@@ -1,4 +1,4 @@
-export const getPrefecturesetPrefectures = async () => {
+export const getPrefectures = async () => {
   const url = "http://localhost:3000/api/prefectures";
   try {
     const response = await fetch(url, {
@@ -7,11 +7,29 @@ export const getPrefecturesetPrefectures = async () => {
         "Content-Type": "application/json",
       },
     });
-
     if (!response.ok) {
       console.error(`Error: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("データの取得に失敗しました:", error);
+    throw error;
+  }
+};
 
+export const getPopulationPerYear = async (id: number) => {
+  const url = `http://localhost:3000/api/population?perYear=${id}`;
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      console.error(`Error: ${response.status} ${response.statusText}`);
+    }
     const data = await response.json();
     return data;
   } catch (error) {
